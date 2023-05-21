bfbc36b00722a76bfa9ee8ea84b18a158c6c46bd
Mohit Choudhary <mrmohit01@gmail.com>
2023-05-22 00:42:37 +0530

    relation operator program

---
+++ relationaloperator.java
@@ -0,0 +1,20 @@
+public class relationaloperator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        
+
+        System.out.println("Number 1 = " + num1);
+        System.out.println("Number 2 = " + num2);
+       
+        System.out.println("Equal : " + (num1==num2));
+        System.out.println("No Equal : " + (num1!=num2));
+        System.out.println("Num1 is Greater than Num2 : " + (num1>num2));
+        System.out.println("Num1 is Less than Num2 : " + (num1<num2));
+
+
+
+        
+    }
+    
+}
