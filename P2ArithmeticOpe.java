f236f9bd0b9c972a0bd61d8ad38fa089927b0ac8
Mohit Choudhary <mrmohit01@gmail.com>
2023-06-01 22:18:02 +0530

    Arithmetic Operator

---
+++ P2ArithmeticOpe.java
@@ -0,0 +1,21 @@
+public class P2ArithmeticOpe {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int mul = a*b;
+        int sum = a+b;
+        int sub = b-a;
+        int div = a/b;
+        
+        //Multiplication
+        System.out.println("Product of " +a+ " and " +b+ " is " + mul);
+        //Sum
+        System.out.println("Sum of " +a+ " and " +b+ " is " + sum);
+        //Substraction
+        System.out.println("Minus of " +a+ " and " +b+ " is " + sub);
+        //Division
+        System.out.println("Division of " +a+ " and " +b+ " is " + div);
+    }
+    
+}
