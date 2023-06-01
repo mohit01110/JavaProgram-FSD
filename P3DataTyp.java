0cbf83e9db6ede91ee0ffaca6a2c5278f01d6895
Mohit Choudhary <mrmohit01@gmail.com>
2023-06-01 22:20:53 +0530

    datatype in java prog

---
+++ P3DataTyp.java
@@ -0,0 +1,22 @@
+public class P3DataTyp {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        String char1 = "Mohit";
+        String char2 = "Choudhary";
+        char text1 = 'm';
+        float num3 = 2.5f; //write f in the last of the number for using float data type
+        //Boolean
+        boolean isEqual1 = num1==num2;
+        System.out.println(isEqual1);
+        //Boolean Character
+        boolean isEqual2 = char1==char2;
+        System.out.println(isEqual2);
+
+        //Using single Character 
+        System.out.println(text1);
+
+        //Using float data type
+        System.out.println(num3);
+    }
+}
