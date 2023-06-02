416bd25bc8481c523bd610ae14ea4781be4bc659
Mohit Choudhary <mrmohit01@gmail.com>
2023-06-02 23:40:43 +0530

    check lenght of variables

---
+++ P14lengthString.java
@@ -0,0 +1,16 @@
+public class P14lengthString {
+    public static void main(String[] args) {
+        
+  
+    //find the length of the string
+    String s1 = new String("baaa");
+    String s2 = new String("aaaa");
+
+    System.out.println(s1.length());
+    System.out.println(s1 + " " +s2);
+    System.out.println(s1.equals(s2));
+    System.out.println(s1.equalsIgnoreCase(s2));
+    System.out.println(s1.compareTo(s2));
+    
+}
+}
