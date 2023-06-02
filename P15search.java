06639fc04df3d536d0edb9612bd40d88a7d119bf
Mohit Choudhary <mrmohit01@gmail.com>
2023-06-02 23:43:01 +0530

    search in variable string

---
+++ P15search.java
@@ -0,0 +1,10 @@
+public class P15search {
+    public static void main(String[] args) {
+        String s1 = "Encyclopaedia";
+        System.out.println(s1.contains("elo"));
+        System.out.println(s1.indexOf("e"));
+        System.out.println(s1.lastIndexOf("l"));
+        System.out.println(s1.substring(2, 7));
+    }
+    
+}
